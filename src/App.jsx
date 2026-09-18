a0229a288484b71db9cf45fcef47c08605df4bb3
Jaybc25 <carlilejb@hotmail.com>
2026-09-18 15:33:39 -0500

    IE-4: add isolated preview route

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import CombinedSummary from "./CombinedSummary.jsx";
 import LoginFrontDoor from "./LoginFrontDoor.jsx";
 import ModelCatalogVisibilityRoute from "./ModelCatalogVisibilityRoute.jsx";
 import HybridSequenceStateTestRoute from "./HybridSequenceStateTestRoute.jsx";
+import InferenceEconomicsPreview from "./InferenceEconomicsPreview.jsx";
 import SharedToolShell from "./SharedToolShell.jsx";
 import "./print-overrides.css";
 
@@ -87,6 +88,19 @@ function ToolRoutes() {
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/tco" element={<TcoRoute />} />
+      <Route
+        path="/tco/inference-economics-preview"
+        element={(
+          <SharedToolShell
+            title="Inference Economics Preview"
+            backHref="/tco"
+            backLabel="TCO Calculator"
+            toolKey="inference-economics-preview"
+          >
+            <InferenceEconomicsPreview />
+          </SharedToolShell>
+        )}
+      />
       <Route
         path="/gpu-sizing"
         element={(
