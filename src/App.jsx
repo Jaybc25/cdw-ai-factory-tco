75abaa9347531257798986c3a95c2d7ce8f923e3
Jaybc25 <carlilejb@hotmail.com>
2026-09-18 21:59:26 -0500

    IE guided: add parallel guided preview route

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import LoginFrontDoor from "./LoginFrontDoor.jsx";
 import ModelCatalogVisibilityRoute from "./ModelCatalogVisibilityRoute.jsx";
 import HybridSequenceStateTestRoute from "./HybridSequenceStateTestRoute.jsx";
 import InferenceEconomicsPreview from "./InferenceEconomicsPreview.jsx";
+import InferenceEconomicsGuidedPreview from "./InferenceEconomicsGuidedPreview.jsx";
 import SharedToolShell from "./SharedToolShell.jsx";
 import "./print-overrides.css";
 
@@ -98,6 +99,19 @@ function ToolRoutes() {
             toolKey="inference-economics-preview"
           >
             <InferenceEconomicsPreview />
+          </SharedToolShell>
+        )}
+      />
+      <Route
+        path="/tco/inference-economics-preview-guided"
+        element={(
+          <SharedToolShell
+            title="Guided Inference Economics Preview"
+            backHref="/tco"
+            backLabel="TCO Calculator"
+            toolKey="inference-economics-preview-guided"
+          >
+            <InferenceEconomicsGuidedPreview />
           </SharedToolShell>
         )}
       />
